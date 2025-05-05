5f89b2c5a54c1d0045fd2b1e20d8f71fd3f4b816
Makramzz <makramehab24@gmail.com>
2025-05-05 03:01:19 +0300

    DashboardPage and DashboardTest update

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -11,7 +11,19 @@ public class DashboardPage extends BasePage {
     private final By employeeDistributionSubUnit = By.xpath("//p[text() = 'Employee Distribution by Sub Unit']");
     private final By employeeDistributionByLocation = By.xpath("//p[text() = 'Employee Distribution by Location']");
     private final By quickLaunchWidget = By.xpath("//p[text() = 'Quick Launch']");
-    private final By buzzFeed = By.xpath("//p[text() = 'Quick Launch']");
+    private final By buzzFeed = By.xpath("//p[text() = 'Buzz Latest Posts']");
+    private final By assignLeaveButton = By.xpath("//button[@title='Assign Leave']//*[name()='svg']");
+    private final By leaveListButton = By.xpath("//button[@title='Leave List']//*[name()='svg']");
+    private final By timeSheetsButton = By.xpath("//button[@title='Timesheets']//*[name()='svg']");
+    private final By applyLeaveButton = By.xpath("//button[@title='Apply Leave']//*[name()='svg']");
+    private final By myLeaveButton = By.xpath("//button[@title='My Leave']//*[name()='svg']");
+    private final By myTimesheetButton = By.xpath("//button[@title='My Timesheet']//*[name()='svg']");
+    private final By assignLeave = By.xpath("//h6[normalize-space()='Assign Leave']");
+    private final By leaveList = By.xpath("//h5[normalize-space()='Leave List']");
+    private final By timesheet = By.xpath("//h6[text()= 'Timesheets Pending Action']");
+    private final By applyLeave = By.xpath("//h6[normalize-space()='Apply Leave']");
+    private final By myLeave = By.xpath("//h5[normalize-space()='My Leave List']");
+    private final By myTimesheet = By.xpath("//h6[normalize-space()='My Timesheet']");
 
 
     public DashboardPage(WebDriver driver) {
@@ -46,5 +58,52 @@ public class DashboardPage extends BasePage {
         return isDisplayed(buzzFeed);
     }
 
+    public boolean isAssignLeaveVisible() {
+        return isDisplayed(assignLeave);
+    }
+
+    public boolean isLeaveListVisible() {
+        return isDisplayed(leaveList);
+    }
+
+    public boolean isTimesheetVisible() {
+        return isDisplayed(timesheet);
+    }
+
+    public boolean isApplyLeaveVisible() {
+        return isDisplayed(applyLeave);
+    }
+
+    public boolean isMyLeaveVisible() {
+        return isDisplayed(myLeave);
+    }
+
+    public boolean isMyTimesheetVisible() {
+        return isDisplayed(myTimesheet);
+    }
+
+    public void assignLeave() {
+        click(assignLeaveButton);
+    }
+
+    public void leaveList() {
+        click(leaveListButton);
+    }
+
+    public void timeSheets() {
+        click(timeSheetsButton);
+    }
+
+    public void applyLeave() {
+        click(applyLeaveButton);
+    }
+
+    public void myLeave() {
+        click(myLeaveButton);
+    }
+
+    public void myTimesheet() {
+        click(myTimesheetButton);
+    }
 
 }
--- src/test/java/tests/DashboardTest.java
+++ src/test/java/tests/DashboardTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 import pages.DashboardPage;
+import pages.MaintenancePage;
 
 import javax.xml.namespace.QName;
 
@@ -16,7 +17,7 @@ public class DashboardTest extends BaseTest {
     @Feature("Dashboard")
     @Story("Dashboard Widgets Visibility")
     @Severity(SeverityLevel.CRITICAL)
-    @Test(description = "Verify all main widgets are visible on the dashboard after login")
+    @Test(description = "Verify all main widgets are visible on the dashboard after login",priority = 1)
     public void testDashboardWidgetsVisibility() {
         Allure.step("Step 1: Log in using valid credentials");
         login();
@@ -53,4 +54,147 @@ public class DashboardTest extends BaseTest {
 
     }
 
+    @Feature("Dashboard")
+    @Story("Quick Launch Links Navigation")
+    @Severity(SeverityLevel.NORMAL)
+    @Test(description = "Verify Assign Leave Link",priority = 2)
+    public void testAssignLeaveLink() {
+        Allure.step("Step 1: Log in using valid credentials");
+        login();
+        Allure.step("Step 2: Initialize Dashboard Page");
+        DashboardPage dashboardPage = new DashboardPage(driver);
+
+
+        Allure.step("Step 3: Verify Dashboard heading is visible");
+        Assert.assertTrue(dashboardPage.isDashboardVisible(), "Dashboard heading is not visible.");
+
+
+        Allure.step("Step 4: Click Assign Leave link");
+        dashboardPage.assignLeave();
+
+
+        Allure.step("Step 5: Verify Assign Leave is visible");
+        Assert.assertTrue(dashboardPage.isAssignLeaveVisible(), "Assign Leave is not visible.");
+
+    }
+
+    @Feature("Dashboard")
+    @Story("Quick Launch Links Navigation")
+    @Severity(SeverityLevel.NORMAL)
+    @Test(description = "Verify Leave List Link",priority = 3)
+    public void testLeaveListLink() {
+        Allure.step("Step 1: Log in using valid credentials");
+        login();
+        Allure.step("Step 2: Initialize Dashboard Page");
+        DashboardPage dashboardPage = new DashboardPage(driver);
+
+
+        Allure.step("Step 3: Verify Dashboard heading is visible");
+        Assert.assertTrue(dashboardPage.isDashboardVisible(), "Dashboard heading is not visible.");
+
+
+        Allure.step("Step 4: Click Leave list link");
+        dashboardPage.leaveList();
+
+
+        Allure.step("Step 5: Verify Assign Leave is visible");
+        Assert.assertTrue(dashboardPage.isLeaveListVisible(), "Leave list is not visible.");
+
+    }
+
+    @Feature("Dashboard")
+    @Story("Quick Launch Links Navigation")
+    @Severity(SeverityLevel.NORMAL)
+    @Test(description = "Verify Time Sheet Link",priority = 4)
+    public void testTimesheetLink() {
+        Allure.step("Step 1: Log in using valid credentials");
+        login();
+        Allure.step("Step 2: Initialize Dashboard Page");
+        DashboardPage dashboardPage = new DashboardPage(driver);
+
+
+        Allure.step("Step 3: Verify Dashboard heading is visible");
+        Assert.assertTrue(dashboardPage.isDashboardVisible(), "Dashboard heading is not visible.");
+
+
+        Allure.step("Step 4: Click My Timesheet link");
+        dashboardPage.timeSheets();
+
+
+        Allure.step("Step 5: Timesheets is visible");
+        Assert.assertTrue(dashboardPage.isTimesheetVisible(), "Timesheet is not visible.");
+
+    }
+
+    @Feature("Dashboard")
+    @Story("Quick Launch Links Navigation")
+    @Severity(SeverityLevel.NORMAL)
+    @Test(description = "Verify Apply Leave Link",priority = 5)
+    public void testApplyLeaveLink() {
+        Allure.step("Step 1: Log in using valid credentials");
+        login();
+        Allure.step("Step 2: Initialize Dashboard Page");
+        DashboardPage dashboardPage = new DashboardPage(driver);
+
+
+        Allure.step("Step 3: Verify Dashboard heading is visible");
+        Assert.assertTrue(dashboardPage.isDashboardVisible(), "Dashboard heading is not visible.");
+
+
+        Allure.step("Step 4: Click Apply Leave link");
+        dashboardPage.applyLeave();
+
+
+        Allure.step("Step 5: Apply Leave is visible");
+        Assert.assertTrue(dashboardPage.isApplyLeaveVisible(), "Apply Leave is not visible.");
+
+    }
+
+    @Feature("Dashboard")
+    @Story("Quick Launch Links Navigation")
+    @Severity(SeverityLevel.NORMAL)
+    @Test(description = "Verify My Leave Link", priority = 6)
+    public void testMyLeaveLink() {
+        Allure.step("Step 1: Log in using valid credentials");
+        login();
+        Allure.step("Step 2: Initialize Dashboard Page");
+        DashboardPage dashboardPage = new DashboardPage(driver);
+
+
+        Allure.step("Step 3: Verify Dashboard heading is visible");
+        Assert.assertTrue(dashboardPage.isDashboardVisible(), "Dashboard heading is not visible.");
+
+
+        Allure.step("Step 4: Click My Leave link");
+        dashboardPage.myLeave();
+
+
+        Allure.step("Step 5: My Leave List is visible");
+        Assert.assertTrue(dashboardPage.isMyLeaveVisible(), "My Leave List is not visible.");
+
+    }
+
+    @Feature("Dashboard")
+    @Story("Quick Launch Links Navigation")
+    @Severity(SeverityLevel.NORMAL)
+    @Test(description = "Verify My Timesheet Link",priority = 7)
+    public void testMyTimesheetLink() {
+        Allure.step("Step 1: Log in using valid credentials");
+        login();
+        Allure.step("Step 2: Initialize Dashboard Page");
+        DashboardPage dashboardPage = new DashboardPage(driver);
+
+
+        Allure.step("Step 3: Verify Dashboard heading is visible");
+        Assert.assertTrue(dashboardPage.isDashboardVisible(), "Dashboard heading is not visible.");
+
+
+        Allure.step("Step 4: Click My Timesheet link");
+        dashboardPage.myTimesheet();
+
+
+        Allure.step("Step 5: My Timesheet is visible");
+        Assert.assertTrue(dashboardPage.isMyTimesheetVisible(), "My Timesheet is not visible.");
+
+    }
 }
